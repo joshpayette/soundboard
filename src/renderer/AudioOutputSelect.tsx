ca36afdebaa0269c8fde2a892e046d98c1455c17
Josh Payette <hello@joshpayette.dev>
2023-08-02 20:35:42 -0400

    Refactored AudioOutputSelect
    Moved logic from the HomeView component to the AudioOutputSelect, where it should have been all along.

--- src/renderer/AudioOutputSelect.tsx
+++ src/renderer/AudioOutputSelect.tsx
@@ -1,14 +1,41 @@
-interface AudioOutputSelectProps {
-  audioOutputDevices: MediaDeviceInfo[];
-  audioOutputDeviceId: string;
-  onAudioOutputDeviceChange: (deviceId: string) => void;
-}
+import { useEffect, useState } from 'react';
 
-export default function AudioOutputSelect({
-  audioOutputDeviceId,
-  audioOutputDevices,
-  onAudioOutputDeviceChange,
-}: AudioOutputSelectProps) {
+/**
+ * Returns a list of audio devices available on the system.
+ */
+export const useListAudioDevices = (): [audioDevices: MediaDeviceInfo[]] => {
+  const [audioDevices, setAudioDevices] = useState<MediaDeviceInfo[]>([]);
+
+  useEffect(() => {
+    async function getAudioDevices() {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const onlyAudioDevices = devices.filter(
+        (device) => device.kind === 'audiooutput'
+      );
+      setAudioDevices(onlyAudioDevices);
+    }
+    getAudioDevices();
+  }, []);
+
+  return [audioDevices];
+};
+
+/**
+ * A select input for choosing an audio output device from a list of available
+ * devices on the system.
+ */
+export function AudioOutputSelect() {
+  const [selectedDeviceId, setSelectedDeviceId] = useState<string>(() =>
+    window.electron.store.get('selectedDeviceId')
+  );
+
+  const [audioDevices] = useListAudioDevices();
+
+  const handleAudioDeviceChange = (deviceId: string) => {
+    setSelectedDeviceId(deviceId);
+    window.electron.store.set('selectedDeviceId', deviceId);
+  };
+
   return (
     <div>
       <label
@@ -20,10 +47,10 @@ export default function AudioOutputSelect({
           id="audioOutputDevice"
           name="audioOutputDevice"
           className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          defaultValue={audioOutputDeviceId}
-          onChange={() => onAudioOutputDeviceChange('')}
+          onChange={(event) => handleAudioDeviceChange(event.target.value)}
+          value={selectedDeviceId}
         >
-          {audioOutputDevices.map((device) => (
+          {audioDevices.map((device) => (
             <option key={device.deviceId} value={device.deviceId}>
               {device.label}
             </option>
--- src/renderer/HomeView.tsx
+++ src/renderer/HomeView.tsx
@@ -1,36 +1,9 @@
-import { useState, useEffect } from 'react';
-import AudioOutputSelect from './AudioOutputSelect';
+import { AudioOutputSelect } from './AudioOutputSelect';
 
 export default function HomeView() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [outputDevices, setOutputDevices] = useState<MediaDeviceInfo[]>([]);
-  const [selectedOutputDeviceId, setSelectedOutputDeviceId] =
-    useState<string>('');
-
-  useEffect(() => {
-    async function getOutputDevices() {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const onlyAudioDevices = devices.filter(
-        (device) => device.kind === 'audiooutput'
-      );
-      console.info(`Found ${onlyAudioDevices.length} audio output devices`);
-      setOutputDevices(onlyAudioDevices);
-    }
-    getOutputDevices();
-  }, []);
-
-  const handleAudioOutputDeviceChange = (deviceId: string) => {
-    console.info(`Selected audio output device: ${deviceId}`);
-    setSelectedOutputDeviceId(deviceId);
-  };
-
   return (
     <div>
-      <AudioOutputSelect
-        audioOutputDevices={outputDevices}
-        audioOutputDeviceId={selectedOutputDeviceId}
-        onAudioOutputDeviceChange={handleAudioOutputDeviceChange}
-      />
+      <AudioOutputSelect />
     </div>
   );
 }
